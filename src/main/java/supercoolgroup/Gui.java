7c686f53fbdcb1be0c3be01e6876a96c44966258
tylernaja <tylernaja@DESKTOP-6GH60NP>
2019-04-03 16:23:15 -0400

    Try to work with FileList.txt

--- src/main/java/supercoolgroup/Gui.java
+++ src/main/java/supercoolgroup/Gui.java
@@ -3,6 +3,7 @@ package supercoolgroup;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.JTree;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ButtonGroup;
@@ -10,10 +11,20 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JRadioButton;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.awt.event.ActionEvent;
 
 public class Gui extends JFrame{
 	
-	JLabel la1, la2, la3, la4;
+	JLabel la1, la2, la4;
 	JTextField t1, t2;
 	JCheckBox ch1;
 	JButton b1,b2,b3;
@@ -21,6 +32,7 @@ public class Gui extends JFrame{
 	ButtonGroup bg1;
 
     DefaultTableModel model = null; // empty table
+    private JTable table;
 
 	public static void main(String[] args) {
 
@@ -33,9 +45,9 @@ public class Gui extends JFrame{
     public Gui() {
 	
 	// Window
-	setLayout(null);
+	getContentPane().setLayout(null);
 	//getContentPane().setBackground(Color.GRAY);
-	setSize(800, 300);
+	setSize(800, 387);
 	setTitle("Search Engine V1.0");
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setResizable(false);
@@ -47,12 +59,12 @@ public class Gui extends JFrame{
 	la1.setFont(new Font("Serif", Font.BOLD, 25));
 	la1.setBounds(100, 10, 400, 30);
 	la1.setForeground(Color.BLACK);
-	add(la1);
+	getContentPane().add(la1);
 	
 	// First TextBox 
 	t1 = new JTextField("");
 	t1.setBounds(200, 70, 400, 20);
-	add(t1);
+	getContentPane().add(t1);
 	
 	// Second Label
 	la2 = new JLabel("Search File :");
@@ -60,51 +72,84 @@ public class Gui extends JFrame{
 	la2.setFont(new Font("Serif", Font.PLAIN, 18));
 	la2.setBounds(100, 62, 100, 30);
 	la2.setForeground(Color.BLACK);
-	add(la2);
-	
-	//Third Label
-	la3 = new JLabel("Files indexed : ");
-	la3.setOpaque(true);
-	la3.setFont(new Font("Serif", Font.BOLD, 13));
-	la3.setBounds(335, 125, 100, 30);
-	la3.setForeground(Color.BLACK);
-	add(la3);
+	getContentPane().add(la2);
 	
 	// Search Button
 	b1 = new JButton("Search");
 	b1.setBounds(200, 120, 100, 30);
-	add(b1);
+	getContentPane().add(b1);
 	
 	//About Button
 	b2 = new JButton("About us");
-	b2.setBounds(20, 235, 100, 20);
-	add(b2);
+	b2.setBounds(10, 327, 100, 20);
+	getContentPane().add(b2);
 	b2.addActionListener(new ActionAbout());
 
 	// Maintenance Button
 	b3 = new JButton("Maintenance");
 	b3.setBounds(470, 120, 130, 30);
-	add(b3);
+	getContentPane().add(b3);
 	b3.addActionListener(new ActionMaintenance(this));
 	
 	rb1 = new JRadioButton("All of the search terms", true);
 	rb1.setBounds(610, 62, 200, 20);
-	add(rb1);
+	getContentPane().add(rb1);
 	
 	rb2 = new JRadioButton("Any of the search terms");
 	rb2.setBounds(610, 80, 200, 30);
-	add(rb2);
+	getContentPane().add(rb2);
 	
 	rb3 = new JRadioButton("Exact Phrase");
 	rb3.setBounds(610, 102, 200, 30);
-	add(rb3);
+	getContentPane().add(rb3);
 	
 	// Radio Group
 	bg1 = new ButtonGroup();
 	bg1.add(rb1);
 	bg1.add(rb2);
 	bg1.add(rb3);
-}
 	
+	JButton btnImportData = new JButton("Import data");
+	btnImportData.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+			String filePath = "C:\\Users\\tylernaja\\git\\COP2805C-SuperCoolGroup\\Assets\\FileList.txt";
+			File file = new File(filePath);
+			
+			try {
+				FileReader fr = new FileReader(file);
+	            BufferedReader br = new BufferedReader(fr);
+	            
+	            DefaultTableModel model = (DefaultTableModel)table.getModel();
+	            Object[] lines = br.lines().toArray();
+	            
+	            for(int i = 0; i < lines.length; i++){
+	                String[] row = lines[i].toString().split(" ");
+	                model.addRow(row);
+	            } 
+			}
+	            catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+			
+		}
+	});
+	btnImportData.setBounds(336, 120, 100, 30);
+	getContentPane().add(btnImportData);
+	
+	JScrollPane scrollPane = new JScrollPane();
+	scrollPane.setBounds(200, 161, 400, 150);
+	getContentPane().add(scrollPane);
+	
+	table = new JTable();
+	table.setModel(new DefaultTableModel(
+		new Object[][] {
+		},
+		new String[] {
+			"File name"
+		}
+	));
+	scrollPane.setViewportView(table);
+}
 }
 
